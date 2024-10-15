0dd34c5bf16ae34da741b1125d597a33c906d8d6
YASHJADON5 <jadonyash2@gmail.com>
2024-10-15 20:10:14 +0530

    fresh react app initialized with shadcn ui

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+
+function App() {
+
+
+  return (
+        <>
+        <Button>Login</Button>
+        </>
+  )
+}
+
+export default App
